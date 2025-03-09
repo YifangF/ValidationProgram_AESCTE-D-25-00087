e4d0d2a3638d9bddb21f77aac01cf894dd3af334
YifangFu <yifangfu@mail.nwpu.edu.cn>
2025-03-09 20:51:30 +0800

    Upload running script of the programs

---
+++ transition_RunProgramScript.m
@@ -0,0 +1,8 @@
+%% Run programs in Scenario 1
+sim("transition_ASMC_2024aR1.slx");
+sim("transition_RLSMC_2024aR1.slx");
+sim("transition_PAFTC_2024aR1.slx");
+%% Run programs in Scenario 2
+sim("transition_ASMC_s2_2024aR1.slx");
+sim("transition_RLSMC_s2_2024aR1.slx");
+sim("transition_PAFTC_s2_2024aR1.slx");
